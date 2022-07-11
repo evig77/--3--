66925bfa375990707406cb67e14e48d60051506b
igor evsikov <evsikov98@mail.ru>
2022-07-11 21:20:52 +0300

    домашнее задание

---
+++ евсиков 3 задание/3:1.swift
@@ -0,0 +1,13 @@
+//
+//  3:1.swift
+//  евсиков 3 задание
+//
+//  Created by Игорь Евсиков on 13.01.2022.
+//
+
+import Foundation
+let a = Int(readLine(strippingNewline: true)!)!
+let b = Int(readLine(strippingNewline: true)!)!
+let c = Int(readLine(strippingNewline: true)!)!
+
+print("\\ a * b * c")
--- евсиков 3 задание/main.swift
+++ евсиков 3 задание/main.swift
@@ -1,11 +1,20 @@
 //
 //  main.swift
-//  евсиков 3 задание
-//
-//  Created by Игорь Евсиков on 12.01.2022.
 //
 
 import Foundation
 
-print("Hello, World!")
+let userSum = readLine()
+var userSum1:Int?
+var userSum2 = userSum1!
+let percent = readLine()
+var percent1:Int?
+var percent2 = percent1!
+var yearsOfcontribution:Int = 5
 
+var totalSum = userSum2 + (userSum2/100*percent2)*yearsOfcontribution
+print("Сумма вклада через 5 лет: \(totalSum)")
+
+
+
+    
